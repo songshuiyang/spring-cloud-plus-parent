b262ae751ffb620c06daa9f329df5e5439099c5f
songshuiyang <1459074711@qq.com>
2019-08-14 18:02:32 +0800

    新增 EnableDiscoveryClient consul注册中心

--- user/src/main/java/com/songsy/springcloud/plus/user/UserApplication.java
+++ user/src/main/java/com/songsy/springcloud/plus/user/UserApplication.java
@@ -2,11 +2,13 @@ package com.songsy.springcloud.plus.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author songsy
  * @date 2019/8/14 17:43
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
 
--- zuul/src/main/java/com/songsy/springcloud/plus/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/songsy/springcloud/plus/zuul/ZuulApplication.java
@@ -2,11 +2,13 @@ package com.songsy.springcloud.plus.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author songsy
  * @date 2019/8/13 18:15
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulApplication {
 
